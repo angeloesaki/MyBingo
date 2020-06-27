fcc13a4abf9a3616b40203a2dc6abff3f403bfe5
angeloesaki <angeloesaki@gmail.com>
2020-06-27 18:04:54 +0900

    すべての配列完成

--- js/main.js
+++ js/main.js
@@ -17,12 +17,17 @@
       source[i] = i + 1 + 15 * col;
     }
 
-    const b = [];
+    const column = [];
     for (let i = 0; i < 5; i++) {
-      b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+      column[i] = source.splice(
+        Math.floor(Math.random() * source.length),
+        1
+      )[0];
     }
 
-    console.log(b);
+    //ランダムで抽出された５つの要素を並べた配列を返す。
+    //例）B配列の場合... column[3, 13, 5, 1, 12]  ←をcreateColumn(0)に返す。そしてcolumns[0]が作られる。
+    return column;
   }
 
   //BINGOのそれぞれのカラムの配列
@@ -37,29 +42,35 @@
   columns[3] = createColumn(3);
   //O
   columns[4] = createColumn(4);
+  //FREEにしたい箇所（真ん中）
+  //columnsの２番目の要素の２つ目の要素
+  columns[2][2] = "FREE";
 
-  //以下、数字が規則的に増えているのでfor文ループで短縮
-  // const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  // const source = [];
-  // for (let i = 0; i < 15; i++) {
-  //   source[i] = i + 1;
-  // }
-
-  //B列の配列
-  // const b = [];
-  //splice()は第一引数に削除し取得したい値の配列のインデックス番号を入れる。第二引数でそこから何個の値を削除し、取得したいかの数字を入れる。
-  //b[0], b[1], b[2]と規則的に数字が増えているので、for文で短縮
-  // b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-
-  // for (let i = 0; i < 5; i++) {
-  //   b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // }
-
-  //spliceの返り値は複数になることもあり、要素が１つでも配列になってしまうため、配列bの中の要素がそれぞれ配列になってしまう。つまり、配列bの中に５個の１つの要素しか持たない配列が入っている状態。
-  //splice()の後に[0]を付ければ解決→なぜ？
-  // console.log(b);
+  //配列の配列のようなデータ構造はconsole.table()で見やすく表示できる
+  console.table(columns);
 }
+
+//以下、数字が規則的に増えているのでfor文ループで短縮
+// const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// const source = [];
+// for (let i = 0; i < 15; i++) {
+//   source[i] = i + 1;
+// }
+
+//B列の配列
+// const b = [];
+//splice()は第一引数に削除し取得したい値の配列のインデックス番号を入れる。第二引数でそこから何個の値を削除し、取得したいかの数字を入れる。
+//b[0], b[1], b[2]と規則的に数字が増えているので、for文で短縮
+// b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+// b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+// b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+// b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+// b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+
+// for (let i = 0; i < 5; i++) {
+//   b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+// }
+
+//spliceの返り値は複数になることもあり、要素が１つでも配列になってしまうため、配列bの中の要素がそれぞれ配列になってしまう。つまり、配列bの中に５個の１つの要素しか持たない配列が入っている状態。
+//splice()の後に[0]を付ければ解決→なぜ？ 配列の何番目の要素の０番目の要素となるから？
+// console.log(b);
